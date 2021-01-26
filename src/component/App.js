67323be815279d1d2f88c95573d5f5f17af17c9d
chris Chen <59293007+githubucl@users.noreply.github.com>
2021-01-26 18:49:07 +1300

    tuesday work on reactredux about song and fav

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import SongDetail from './SongDetail';
+import SongList from './SongList'
 
 function App() {
   return (
-    <div className="App">
-      hi
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/component/SongDetail.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { connect } from "react-redux";
+
+const SongDetail = ({ song }) => {
+    return (
+        <div>
+            {song.title}
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return { song: state.selectedSong }
+}
+
+export default connect(mapStateToProps)(SongDetail)
